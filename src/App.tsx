1df9d50358678c021d49ddc916553eaa4a8ab231
fabiangamboa95 <fabiangamboa95@gmail.com>
2021-06-29 22:40:13 -0400

    playable

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import produce from "immer";
+
 const App = () => {
-  return <div className=""></div>;
+  const side = 4;
+  const [grid, setGrid] = useState(Array(side ** 2).fill(false));
+
+  const onClick = (index: number) => {
+    const row = Math.floor(index / side);
+    const column = index % side;
+    setGrid(
+      produce(grid, (draft) => {
+        for (let i = 0; i < side; i++) {
+          draft[row * side + i] = !draft[row * side + i];
+          draft[i * side + column] = !draft[i * side + column];
+          draft[index] = !draft[index];
+        }
+      })
+    );
+  };
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div className={`grid grid-cols-${side} gap-3`}>
+        {grid.map((value, index) => (
+          <div
+            className={`${
+              value ? "bg-yellow-400" : "bg-black"
+            } rounded-full w-10 h-10`}
+            key={index}
+            onClick={() => onClick(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
